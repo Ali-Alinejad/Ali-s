5243578c5ee6e92bdfa33851b3e865a41c44e97b
WINFORD <winford.op@gmail.com>
2023-12-21 13:51:27 +0330

    starting with resturant

--- javascripts/turorials/js.js
+++ javascripts/turorials/js.js
@@ -1,2 +1,25 @@
-const resturant = [(nickname = "Anna")];
-console.log(resturant);
+/* const WINFORD = {
+  firstname: "Ali",
+  lastname: " Alinejad",
+  age: 23,
+};
+const Annabela = {
+  firstname: "anna",
+  lastname: " ebrahimian",
+  age: 19,
+};
+const newanna = Object.assign({}, Annabela);
+newanna.lastname = WINFORD.lastname;
+
+console.log("family alan =", newanna.lastname);
+console.log("va qabli is = ", Annabela.lastname);
+ */
+
+"use strict";
+const resturant = {
+  name: "italiano",
+  location: "manhattan,NY",
+  Categories: ["italiano", "pizza", "pasta", "organic"],
+  starting: ["Ash", "sowp", "saladshirzi"],
+  mainmenu: ["pizaa", "joje", "kababkobide"],
+};
