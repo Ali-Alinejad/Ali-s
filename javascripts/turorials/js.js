1f66fbaa6fd7e7a073474ad2d5935eea65afef6b
WINFORD <winford.op@gmail.com>
2023-12-22 15:42:36 +0330

    chalange with football

--- javascripts/turorials/js.js
+++ javascripts/turorials/js.js
@@ -23,3 +23,10 @@ const resturant = {
   starting: ["Ash", "sowp", "saladshirzi"],
   mainmenu: ["pizaa", "joje", "kababkobide"],
 };
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(resturant.starting[0]);
