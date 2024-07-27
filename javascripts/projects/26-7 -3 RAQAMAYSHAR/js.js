22363ead6e8159747d399617195ca9fd2ab34d38
WINFORD <winford.op@gmail.com>
2024-07-27 13:22:08 +0330

    set random color

--- javascripts/projects/26-7 -3 RAQAMAYSHAR/js.js
+++ javascripts/projects/26-7 -3 RAQAMAYSHAR/js.js
@@ -1,12 +1,3 @@
-document
-  .querySelector(".changebtn")
-  .addEventListener("click", function separate(Number) {
-    Number += "";
-    Number = Number.replace(",", "");
-    x = Number.split(".");
-    y = x[0];
-    z = x.length > 1 ? "." + x[1] : "";
-    var rgx = /(\d+)(\d{3})/;
-    while (rgx.test(y)) y = y.replace(rgx, "$1" + "," + "$2");
-    document.querySelector("show").textContent = y + z;
-  });
+document.querySelector(".mainsection").addEventListener(`keyup`, function () {
+  document.getElementById("changebtn").value = "1";
+});
