d13562bebcef2c160101703864bb5665abf85a5e
WINFORD <winford.op@gmail.com>
2024-07-29 18:24:15 +0330

    set js code for learning timers

---
+++ javascripts/projects/29-7-timer/js.js
@@ -0,0 +1,26 @@
+const clickstart = document.querySelector(".startbtn");
+const clickstop = document.querySelector(".stpbtn");
+const label1 = document.querySelector(".label");
+let s = 0;
+let m = 0;
+let h = 0;
+
+clickstart.addEventListener("click", function () {
+  timerr = setInterval(function () {
+    s += 1;
+
+    if (s == 60) {
+      m += 1;
+      s = 0;
+    } else if (m == 60) {
+      h += 1;
+      m = 0;
+      s = 0;
+    }
+    label1.textContent = h + ":" + m + ":" + s;
+  }, 1000);
+});
+
+clickstop.addEventListener("click", function () {
+  clearInterval(timerr);
+});
