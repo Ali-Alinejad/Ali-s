48aa3cefd9995670c4c2055a23737209f5e9d539
WINFORD <winford.op@gmail.com>
2024-08-04 11:24:09 +0330

    set js

--- javascripts/projects/2-8-shoppage/data/main.js
+++ javascripts/projects/2-8-shoppage/data/main.js
@@ -2,7 +2,9 @@ document.querySelector(".slide").addEventListener("click", function () {
   document.getElementById("logo").style.backgroundImage =
     'URL("../pic/00001209.jpg")';
 });
-
-document.querySelector("menu1").addEventListener("click", (e) => {
-  document.getElementById("menu1").textContent = "close";
+document.querySelector(".leftbar").addEventListener("click", function () {
+  document.getElementById("menu1").textContent = "";
+});
+document.getElementById("closemenu").addEventListener("click", function () {
+  document.getElementById("menu1").textContent = "menu";
 });
