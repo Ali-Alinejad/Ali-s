169c41deaf09ac0624a80091196c2617a96bcb19
WINFORD <winford.op@gmail.com>
2024-08-04 12:39:35 +0330

    get js side for tranfrom

--- javascripts/projects/2-8-shoppage/data/main.js
+++ javascripts/projects/2-8-shoppage/data/main.js
@@ -1,10 +1,19 @@
+document.getElementById("sideitems").style.transform = "translateX(-00px)";
+
+const mode = document.querySelector(".body");
+
 document.querySelector(".slide").addEventListener("click", function () {
   document.getElementById("logo").style.backgroundImage =
     'URL("../pic/00001209.jpg")';
 });
-document.querySelector(".leftbar").addEventListener("click", function () {
-  document.getElementById("menu1").textContent = "";
-});
-document.getElementById("closemenu").addEventListener("click", function () {
+function closenav() {
   document.getElementById("menu1").textContent = "menu";
-});
+  document.getElementById("sideitems").style.transform = "translateX(-400px)";
+  document.getElementById("sideitems").style.transition = "1s";
+}
+function openNav() {
+  document.getElementById("menu1").textContent = " ";
+  document.getElementById("sideitems").style.transform = "translateX(0px)";
+  document.getElementById("sideitems").style.transition = "1s";
+  document.getElementById("sideitems").blur = "true";
+}
